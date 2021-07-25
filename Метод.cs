f6e1fe40dbf58f9e55ce0043ed5cfd6e4f511330
SkillFactoryCSharpTest <anton-zhemaev@rambler.ru>
2021-07-25 21:50:38 +0300

    Изучение модуля 5.1

---
+++ Метод.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1st_lesson
+{
+    class Programm
+    {
+
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int min = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int y = i + 1; y < result.Length; y++)
+                {
+                    if (result[i] > result[y])
+                    {
+                        min = result[i];
+
+                        result[i] = result[y];
+
+                        result[y] = min;
+                    }
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            GetArrayFromConsole();
+
+        }
+
+    }
+}
