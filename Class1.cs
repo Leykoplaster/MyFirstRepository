98ca1327098a1968a70f48c51a239fdc54e0eb96
SkillFactoryCSharpTest <anton-zhemaev@rambler.ru>
2021-07-29 14:06:56 +0300

    Сделал Showcolors

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class MainClass
+{
+
+    static string ShowColor()
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+        return color;
+    }
+
+    public static void Main(string[] args)
+    {
+        var favcolors = new string[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            favcolors[i] = ShowColor();
+        }
+
+        foreach (var color in favcolors)
+        {
+            Console.WriteLine(color);
+        }
+
+        Console.ReadKey();
+
+
+    }
+}
